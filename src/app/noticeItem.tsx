0faa3c96d83b6259c732a3139e77ebb4459c2a3a
goeun208 <qop341@gmail.com>
2023-07-21 16:52:27 +0900

    delete 기능 추가

--- src/app/noticeItem.tsx
+++ src/app/noticeItem.tsx
@@ -1,10 +1,8 @@
 "use client"
-
 import Link from "next/link";
-import { useSetRecoilState, useRecoilValue } from "recoil";
-import { noticeId } from "./recoil/notice";
-import { useEffect } from "react";
 import { graphql } from "@/gql";
+import { UUID } from "crypto";
+import request from 'graphql-request'
 
 const deleteNotice: any = graphql(/* GraphQL */ `
         mutation deleteNotice(
@@ -19,12 +17,30 @@ const deleteNotice: any = graphql(/* GraphQL */ `
     `)
 
 const NoticeItem = ({e}:any) => {
+    const API_URL: any = process.env.NEXT_PUBLIC_API_URL;
+    const deleteMutation: any =
+       (_id: UUID) => {
+        request(
+                API_URL,
+                deleteNotice,
+                { id: _id},
+                {
+                    'X-Hasura-Admin-Secret': 'aniauto-dev',
+                }
+            )          
+       }
+
+    const deleteFn = (id: UUID) => {
+        //데이터 삭제
+        deleteMutation(id)
+        alert('삭제되었습니다.')
+    }
+
     return (
         <div className="flex w-[40rem] justify-between items-center oevrflow-hidden py-3 px-4">
             <div className="w-[30rem]">
                 <div className="w-[6rem] rounded-2xl mb-3 text-center border border-[#5048e5]">{e.category}</div>
                 <div><span className="text-[#5048e5] font-bold">{e.title}</span></div>
-                {/* <input type="text" defaultValue={title} onChange={(e) => handleChangeTitle(e)} className=""/> */}
                 <div>{e.contents}</div>
             </div>
             <div>
@@ -32,7 +48,7 @@ const NoticeItem = ({e}:any) => {
                     <button type="button" className="border bg-sky-300 px-2 rounded-md mr-2">수정</button>
                 </Link>
                 
-                <button type="button" className="border bg-red-300 px-2 rounded-md">삭제</button>
+                <button type="button" className="border bg-red-300 px-2 rounded-md" onClick={() => deleteFn(e.id)}>삭제</button>
             </div>
             
         </div>
